dbd8b520cfeebfe0b726d901e1b7bb8f263c9a99
xiongyx <1399852153@qq.com>
2019-06-21 11:08:49 +0800

    实现IOC

--- toy_spring_mvc/src/main/java/com/xiongyx/annotation/MyPathVariable.java
+++ toy_spring_mvc/src/main/java/com/xiongyx/annotation/MyPathVariable.java
@@ -8,6 +8,9 @@ import java.lang.annotation.Target;
 /**
  * @author xiongyx
  * @date 2019/6/11
+ *
+ * todo MyPathVariable 涉及到动态url匹配，比较复杂，暂不支持
+ * todo 处理逻辑可以参考spring-core中的 AntPathMatcher
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
