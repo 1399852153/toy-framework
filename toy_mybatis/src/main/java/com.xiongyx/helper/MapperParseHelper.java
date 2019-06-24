8fda62af177c3751e262882338ec338f15ca8f6f
xiongyx <1399852153@qq.com>
2019-06-24 11:35:14 +0800

    乞丐版mybatis

--- toy_mybatis/src/main/java/com.xiongyx/helper/MapperParseHelper.java
+++ toy_mybatis/src/main/java/com.xiongyx/helper/MapperParseHelper.java
@@ -2,16 +2,8 @@ package com.xiongyx.helper;
 
 import com.xiongyx.constant.Constant;
 import com.xiongyx.model.MappedStatement;
-import com.xiongyx.util.XmlUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.dom4j.Document;
 import org.dom4j.Element;
-import java.io.File;
-import com.xiongyx.constant.Constant.SqlType;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author xiongyx
@@ -21,55 +13,34 @@ import java.util.List;
  */
 public class MapperParseHelper {
 
-    static{
-        List<Statement> statementList = new ArrayList<>();
+    public static MappedStatement parseMappedStatement(String namespace, Element statementElement){
+        String eleName = statementElement.getName();
 
-        Document document = XmlUtil.readXml(new File("D:\\github\\toy-framework\\demo\\src\\main\\resources\\mapper\\UserMapper.xml"));
+        MappedStatement statement = new MappedStatement();
 
-        // 获取xml中的根元素
-        Element rootElement = document.getRootElement();
-
-        // 不是beans根元素的，文件不对
-        if (!Constant.XML_ROOT_LABEL.equals(rootElement.getName())) {
-            System.err.println("mapper xml文件根元素不是mapper");
+        if (Constant.SqlType.SELECT.value().equals(eleName)) {
+            String resultType = statementElement.attributeValue(Constant.XML_SELECT_RESULTTYPE);
+            statement.setResultType(resultType);
+            statement.setSqlCommandType(Constant.SqlType.SELECT);
+        }
+        else if (Constant.SqlType.UPDATE.value().equals(eleName)) {
+            statement.setSqlCommandType(Constant.SqlType.UPDATE);
+        }
+        else {
+            // 其他标签自己实现
+            System.err.println("不支持此xml标签解析:" + eleName);
+            statement.setSqlCommandType(Constant.SqlType.DEFAULT);
         }
 
-        String namespace = rootElement.attributeValue(Constant.XML_SELECT_NAMESPACE);
+        //设置SQL的唯一ID
+        String sqlId = namespace + "." + statementElement.attributeValue(Constant.XML_ELEMENT_ID);
 
-        List<MappedStatement> statements = new ArrayList<>();
-        for (Iterator iterator = rootElement.elementIterator(); iterator.hasNext();) {
-            Element element = (Element)iterator.next();
-            String eleName = element.getName();
+        statement.setSqlId(sqlId);
+        statement.setNameSpace(namespace);
+        statement.setSqlSource(StringUtils.trim(statementElement.getStringValue()));
 
-            MappedStatement statement = new MappedStatement();
+        System.out.println(statement);
 
-            if (SqlType.SELECT.value().equals(eleName)) {
-                String resultType = element.attributeValue(Constant.XML_SELECT_RESULTTYPE);
-                statement.setResultType(resultType);
-                statement.setSqlCommandType(SqlType.SELECT);
-            }
-            else if (SqlType.UPDATE.value().equals(eleName)) {
-                statement.setSqlCommandType(SqlType.UPDATE);
-            }
-            else {
-                // 其他标签自己实现
-                System.err.println("不支持此xml标签解析:" + eleName);
-                statement.setSqlCommandType(SqlType.DEFAULT);
-            }
-
-            //设置SQL的唯一ID
-            String sqlId = namespace + "." + element.attributeValue(Constant.XML_ELEMENT_ID);
-
-            statement.setSqlId(sqlId);
-            statement.setNameSpace(namespace);
-            statement.setSqlSource(StringUtils.trim(element.getStringValue()));
-            statements.add(statement);
-
-            System.out.println(statement);
-//            configuration.addMappedStatement(sqlId, statement);
-//
-//            //这里其实是在MapperRegistry中生产一个mapper对应的代理工厂
-//            configuration.addMapper(Class.forName(namespace));
-        }
+        return statement;
     }
 }
--- toy_mybatis/src/main/java/com.xiongyx/model/Configuration.java
+++ toy_mybatis/src/main/java/com.xiongyx/model/Configuration.java
@@ -1,5 +1,14 @@
 package com.xiongyx.model;
 
+import com.xiongyx.constant.Constant;
+import com.xiongyx.helper.MapperParseHelper;
+import com.xiongyx.util.XmlUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -10,7 +19,31 @@ import java.util.Map;
  */
 public class Configuration {
 
-    private static Map<String,MappedStatement> mappedStatementMap;
+    private static Map<String,MappedStatement> mappedStatementMap = new HashMap<>();
 
+    static{
+        Document document = XmlUtil.readXml(new File("D:\\github\\toy-framework\\demo\\src\\main\\resources\\mapper\\UserMapper.xml"));
 
+        // 获取xml中的根元素
+        Element rootElement = document.getRootElement();
+
+        // 不是beans根元素的，文件不对
+        if (!Constant.XML_ROOT_LABEL.equals(rootElement.getName())) {
+            System.err.println("mapper xml文件根元素不是mapper");
+        }
+
+        String namespace = rootElement.attributeValue(Constant.XML_SELECT_NAMESPACE);
+
+        for (Iterator iterator = rootElement.elementIterator(); iterator.hasNext();) {
+            Element element = (Element)iterator.next();
+            MappedStatement mappedStatement = MapperParseHelper.parseMappedStatement(namespace,element);
+
+            // 存入map映射
+            MappedStatement sameKey = mappedStatementMap.put(mappedStatement.getSqlId(),mappedStatement);
+            if(sameKey != null){
+                // 发现存在相同sqlID的 mappedStatement
+                throw new RuntimeException("has same sqlID sqlId=" + mappedStatement.getSqlId());
+            }
+        }
+    }
 }
