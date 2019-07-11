fd8a33ede13e7ab0527c7868eb1c71bc7211aae5
zhangjianwei <CSSVDD@gmail.com>
2019-07-11 20:21:44 +0800

    resultMap映射

--- toy_mybatis/src/main/java/com.xiongyx/executor/resultset/DefaultResultSetHandler.java
+++ toy_mybatis/src/main/java/com.xiongyx/executor/resultset/DefaultResultSetHandler.java
@@ -5,12 +5,15 @@ package com.xiongyx.executor.resultset;
 
 
 import com.xiongyx.model.MappedStatement;
+import com.xiongyx.util.JsonUtil;
 
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 
 /**
@@ -46,30 +49,58 @@ public class DefaultResultSetHandler implements ResultSetHandler
                 return null;
             }
 
+
+
+            // Class<E> eClass = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+            Class<?> eClass = Class.forName(mappedStatement.getResultType());
+
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
             while (resultSet.next()) {
-                // 通过反射实例化返回类
-                Class<?> entityClass = Class.forName(mappedStatement.getResultType());
-                E entity = (E)entityClass.newInstance();
-                Field[] declaredFields = entityClass.getDeclaredFields();
-
-                for (Field field : declaredFields) {
-                    // 对成员变量赋值
-                    field.setAccessible(true);
-                    Class<?> fieldType = field.getType();
-
-                    // 目前只实现了string和int转换
-                    if (String.class.equals(fieldType)) {
-                        field.set(entity, resultSet.getString(field.getName()));
-                    } else if (int.class.equals(fieldType) || Integer.class.equals(fieldType)) {
-                        field.set(entity, resultSet.getInt(field.getName()));
-                    } else {
-                        // 其他类型自己转换，这里就直接设置了
-                        field.set(entity, resultSet.getObject(field.getName()));
+                E entity = (E) eClass.newInstance();
+                for (int i = 0; i < metaData.getColumnCount(); i++) {
+                    String columnLabel = metaData.getColumnLabel(i + 1);
+                    Object columnValue = resultSet.getObject(columnLabel);
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put(columnLabel, columnValue);
+                    System.out.println(JsonUtil.objectToJsonString(map));
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+                        String fieldName = entry.getKey();
+                        Object fieldValue = entry.getValue();
+                        Field field = eClass.getDeclaredField(fieldName);
+                        field.setAccessible(true);
+                        field.set(entity, fieldValue);
                     }
                 }
-
                 result.add(entity);
             }
+
+
+            //
+            // while (resultSet.next()) {
+            //     // 通过反射实例化返回类
+            //     Class<?> entityClass = Class.forName(mappedStatement.getResultType());
+            //     E entity = (E)entityClass.newInstance();
+            //     Field[] declaredFields = entityClass.getDeclaredFields();
+            //
+            //     for (Field field : declaredFields) {
+            //         // 对成员变量赋值
+            //         field.setAccessible(true);
+            //         Class<?> fieldType = field.getType();
+            //
+            //         // 目前只实现了string和int转换
+            //         if (String.class.equals(fieldType)) {
+            //             field.set(entity, resultSet.getString(field.getName()));
+            //         } else if (int.class.equals(fieldType) || Integer.class.equals(fieldType)) {
+            //             field.set(entity, resultSet.getInt(field.getName()));
+            //         } else {
+            //             // 其他类型自己转换，这里就直接设置了
+            //             field.set(entity, resultSet.getObject(field.getName()));
+            //         }
+            //     }
+            //
+            //     result.add(entity);
+            // }
 
             return result;
         }
