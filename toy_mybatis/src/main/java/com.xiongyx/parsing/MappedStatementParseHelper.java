26166c253b48c0ef271686768722752cc5795093
xiongyx <1399852153@qq.com>
2019-07-17 18:36:33 +0800

    乞丐版mybatis 动态sql解析

--- toy_mybatis/src/main/java/com.xiongyx/parsing/MappedStatementParseHelper.java
+++ toy_mybatis/src/main/java/com.xiongyx/parsing/MappedStatementParseHelper.java
@@ -17,7 +17,12 @@ public class MappedStatementParseHelper {
     public static SqlSource parseSqlUnit(Element sqlUnitNode){
         // todo
 
-        //
+        // rootNode是一个MixedNode，持有一个 contentsList
+        // 获取sqlUnitNode的所有一级孩子节点,判断孩子节点类型
+            // 文本节点 ==> TextSqlNode 加入contentsList
+            // 非文本节点
+                // 根据当前节点的名称决定对应的SqlNodeHandler，handler能够递归的解析目前节点的孩子节点
+        // 将最终的 contentsList存入rootNode，并构造对应的SqlSource
 
         return null;
     }
---
+++ toy_mybatis/src/main/java/com.xiongyx/scripting/sqlnode/TextSqlNode.java
@@ -0,0 +1,14 @@
+package com.xiongyx.scripting.sqlnode;
+
+/**
+ * @author xiongyx
+ * @date 2019/7/17
+ *
+ * 文本节点
+ */
+public class TextSqlNode implements SqlNode{
+    @Override
+    public void apply(DynamicSqlParseContext context) {
+
+    }
+}
