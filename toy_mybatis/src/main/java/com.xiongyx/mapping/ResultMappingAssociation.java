c48eb16229c9c2678198c548f301833da858d63c
1399852153 <139985215.@qq.com>
2019-08-07 21:59:09 +0800

    乞丐版 mybatis 实现嵌套resultMap

---
+++ toy_mybatis/src/main/java/com.xiongyx/mapping/ResultMappingAssociation.java
@@ -0,0 +1,42 @@
+package com.xiongyx.mapping;
+
+import java.util.List;
+
+/**
+ * @author xiongyx
+ * on 2019/8/7.
+ *
+ * 嵌套resultMap association
+ */
+public class ResultMappingAssociation extends ResultMapping{
+
+    /**
+     * 映射的类型
+     * */
+    private String type;
+
+    /**
+     * 内部复合的 字段映射集合
+     * */
+    private List<ResultMapping> compositeResultMappingList;
+    
+    public ResultMappingAssociation(String column, String property, String jdbcType) {
+        super(column, property, jdbcType);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<ResultMapping> getCompositeResultMappingList() {
+        return compositeResultMappingList;
+    }
+
+    public void setCompositeResultMappingList(List<ResultMapping> compositeResultMappingList) {
+        this.compositeResultMappingList = compositeResultMappingList;
+    }
+}
