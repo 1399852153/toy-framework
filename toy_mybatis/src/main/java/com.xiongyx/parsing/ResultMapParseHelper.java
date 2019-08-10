eac3f464590a5b26e77cc028afb0ba5eba15cd69
xiongyx <1399852153@qq.com>
2019-08-10 10:50:12 +0800

    实现嵌套resultMap

--- toy_mybatis/src/main/java/com.xiongyx/parsing/ResultMapParseHelper.java
+++ toy_mybatis/src/main/java/com.xiongyx/parsing/ResultMapParseHelper.java
@@ -1,7 +1,6 @@
 package com.xiongyx.parsing;
 
 import com.xiongyx.mapping.*;
-import org.omg.CORBA.TRANSACTION_MODE;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
