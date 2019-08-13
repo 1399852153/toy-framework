98880134c5527e0699121764874a2b0286c927d4
1399852153 <139985215.@qq.com>
2019-08-13 22:16:27 +0800

    乞丐版 mybatis 实现嵌套resultMap 调试

--- demo/src/main/resources/sql/customer.sql
+++ demo/src/main/resources/sql/customer.sql
@@ -1,33 +1,84 @@
--- 用户表
-create table test_user(
-	id varchar(32) not null COMMENT '主键 id',
+/*
+Navicat MySQL Data Transfer
 
-	userName varchar(20) not null COMMENT '姓名',
-	age int not null COMMENT '年龄',
-  money int not null COMMENT '存款',
-	primary key(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+Source Server         : localhost_3306
+Source Server Version : 50710
+Source Host           : localhost:3306
+Source Database       : toy_framework
 
--- 书籍表
-DROP TABLE IF EXISTS `test_book`;
-create table test_book(
-	id varchar(32) not null COMMENT '主键 id',
+Target Server Type    : MYSQL
+Target Server Version : 50710
+File Encoding         : 65001
 
-	book_name varchar(20) not null COMMENT '书名',
-	author_id varchar(32) not null COMMENT '作者id',
-  price int not null COMMENT '价格',
-	primary key(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+Date: 2019-08-13 22:15:43
+*/
 
--- 帐号表
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for test_account
+-- ----------------------------
 DROP TABLE IF EXISTS `test_account`;
-create table test_account(
-	id varchar(32) not null COMMENT '主键 id',
+CREATE TABLE `test_account` (
+`id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键 id' ,
+`user_id`  varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id' ,
+`password`  int(11) NOT NULL COMMENT '余额' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci
 
-	user_id varchar(20) not null COMMENT '用户id',
-  password int not null COMMENT '余额',
-	primary key(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+;
 
+-- ----------------------------
+-- Records of test_account
+-- ----------------------------
+BEGIN;
+INSERT INTO `test_account` VALUES ('12312', 'aaaaa', '123456'), ('321312', 'bbbbb', '123456');
+COMMIT;
 
+-- ----------------------------
+-- Table structure for test_book
+-- ----------------------------
+DROP TABLE IF EXISTS `test_book`;
+CREATE TABLE `test_book` (
+`id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键 id' ,
+`book_name`  varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '书名' ,
+`author_id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '作者id' ,
+`price`  int(11) NOT NULL COMMENT '价格' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci
 
+;
+
+-- ----------------------------
+-- Records of test_book
+-- ----------------------------
+BEGIN;
+INSERT INTO `test_book` VALUES ('111111', 'aaaaa书籍1', 'aaaaa', '100'), ('222222', 'aaaaa书籍2', 'aaaaa', '200'), ('333333', 'bbbbb书籍1', 'bbbbb', '100'), ('555555', 'bbbbb书籍2', 'bbbbb', '200');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for test_user
+-- ----------------------------
+DROP TABLE IF EXISTS `test_user`;
+CREATE TABLE `test_user` (
+`id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键 id' ,
+`userName`  varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名' ,
+`age`  int(11) NOT NULL COMMENT '年龄' ,
+`money`  int(11) NOT NULL COMMENT '存款' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+;
+
+-- ----------------------------
+-- Records of test_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `test_user` VALUES ('aaaaa', '打扫打扫打扫', '12', '32131'), ('bbbbb', '打扫打扫大', '13', '32131');
+COMMIT;
